183204587b9490c3e86990f52ec17109086a1ee8
ananth-mask <78956460+ananth-mask@users.noreply.github.com>
2021-02-12 15:30:05 +0530

    Add files via upload

---
+++ area of triangle.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <math.h>
+double areaoftri( double a,double b,double c);
+void main()
+{
+	double n1,n2,n3,area;
+	printf("enter the three sides ");
+	scanf("%lf %lf %lf",&n1,&n2,&n3);
+	area=areaoftri(n1,n2,n3);
+	printf(" the area of triangle is: %.2lf",area);
+}
+
+double areaoftri(double a,double b,double c)
+{
+	double s,area;
+	s=(a+b+c)/2;
+	area= sqrt(s*(s-a)*(s-b)*(s-c));
+	return area;
+}
+	
---
+++ average .c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+int average(int , int , int );
+void main()
+{
+	int n1,n2,n3,avg;
+	printf("enter three nunbers ");
+	scanf(" %d %d %d", &n1,&n2,&n3);
+	avg=average( n1,n2,n3);
+	printf("the average is: %d", avg);
+}
+
+int average(int a,int b,int c)
+{
+	double area;
+	area= (a+b+c)/3 ;
+	return area;
+}
